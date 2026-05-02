c18d18110b7da2088651870719fb63a962647999
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-05-02 21:38:32 +0400

    roboshop v3 updated with variables

--- resources/variables.tf
+++ resources/variables.tf
@@ -10,14 +10,9 @@ variable "vpc_sg_id" {
   default = ["sg-0fcfe62979bdd0bf1"]
 }
 
-variable "s3record" {
-  default = "aws_route53_record"
-}
-
 variable "zone_id" {
   default = "Z01214421PKKTLXAI5VN5"
 }
-
 
 variable "dns_type" {
   default = "A"
